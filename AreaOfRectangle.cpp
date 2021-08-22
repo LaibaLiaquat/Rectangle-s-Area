3c028fe3a98f74f29557d3997cc3445c8725d203
LaibaLiaquat <89074111+LaibaLiaquat@users.noreply.github.com>
2021-08-22 19:00:19 +0500

    Add files via upload

--- AreaOfRectangle.cpp
+++ AreaOfRectangle.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int length, width, area;
+	cout << "Enter the length of Rectangle:\n";
+	cin >> length;
+	cout << "Enter the width of Rectangle:\n";
+	cin >> width;
+	area = length * width;
+	cout << "Area of Rectangle is:" << area << endl;
+	return 0;
+}
